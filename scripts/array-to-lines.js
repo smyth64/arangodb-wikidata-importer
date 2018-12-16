21fb9b90145d45c32e3e6d22f9454190d9ff3279
smyth <smyth@smyths-MacBook-Pro.local>
2018-12-16 13:49:31 +0100

    update

--- scripts/array-to-lines.js
+++ scripts/array-to-lines.js
@@ -10,7 +10,7 @@ var args = process.argv.slice(2);
 var input = args[0];
 var output = args[1];
 
-rimraf(output + '/*', function(error) {
+rimraf(output, function(error) {
   stream.output.on("data", function(object){
       fs.appendFileSync(output, JSON.stringify(object.value) + '\n')
   });
