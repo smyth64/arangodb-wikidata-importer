74673990b6b4e4cee4bdee1340ca39ffab0d4af1
smyth <smyth@smyths-MacBook-Pro.local>
2018-12-16 13:47:08 +0100

    delete output file of array to lines

--- scripts/array-to-lines.js
+++ scripts/array-to-lines.js
@@ -9,17 +9,20 @@ var args = process.argv.slice(2);
 var input = args[0];
 var output = args[1];
 
-stream.output.on("data", function(object){
-    fs.appendFileSync(output, JSON.stringify(object.value) + '\n')
-});
+rimraf(output + '/*', function(error) {
+  stream.output.on("data", function(object){
+      fs.appendFileSync(output, JSON.stringify(object.value) + '\n')
+  });
+  
+  stream.output.on("end", function(){
+    console.log("done");
+    process.exit();
+  });
+  
+  if (args.length > 1)
+    fs.createReadStream(input).pipe(stream.input);
+  
+  else
+    console.log('Please provide input AND output file!');
+})
 
-stream.output.on("end", function(){
-  console.log("done");
-  process.exit();
-});
-
-if (args.length > 1)
-  fs.createReadStream(input).pipe(stream.input);
-
-else
-  console.log('Please provide input AND output file!');
