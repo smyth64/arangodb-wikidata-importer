c7b885c392044c7e9d561f5ef48f52f75270b853
smyth <smyth@smyths-MacBook-Pro.local>
2018-12-16 02:34:57 +0100

    update

--- README.md
+++ README.md
@@ -1,20 +1,30 @@
 Hey!
 
-How to import wikidata dump to ArangoDB?
+How to import wikidata dump to ArangoDB in 5 simple steps?
 
-First of all we convert this huuuuuge Array of the dump to a format, where each object is in a new line.
+1. First of all we convert this huuuuuge Array of the dump to a format, where each object is in a new line.
 ```
 node scripts/array-to-lines.js dump/latest-all.json dump/latest-all-lines.json
 ```
 
-Then we split the file in 4 (recommended number is the amount of CPU cores you have) parts in order so that we can use one CPU for each file.
+2. Now you can parse each file seperately using this script
+```
+node multi-files-parser.js splitted/*
+```
+
+3. Then we split the file in 4 (recommended number is the amount of CPU cores you have) parts in order so that we can use one CPU for each file.
 
 ```
-node scripts/split-file.js dump/latest-all-lines.json 4 splitted
+node scripts/split-file.js dump/latest-all-lines.json 4
 ```
 
-So now we can import everything! 😋
+4. Out ArangoDB is already waiting hungry for the data 😄 Let's start it
+```
+docker-compose up -d
+```
+
+5. So now we can import everything! 😋
 
 ```
-docker-compose exec -it arangodb node /scripts/importer.js /testdata/*
+docker-compose exec -it arangodb node /scripts/importer.js /parsed
 
--- scripts/importer.js
+++ scripts/importer.js
@@ -15,119 +15,127 @@ var db = require('arangojs')({
   url: url
 });
 
-var args = process.argv.slice(2);
+const args = process.argv.slice(2);
+// const filesPath = '/parsed/*';
 var filesPath = args[0];
+console.log (args)
 
-if (filesPath) {
-  try {
-    fs.accessSync(filesPath, fs.F_OK);
-  } catch (e) {
-    console.log("Given File does not exist or is not readable!");
-    process.exit();
-  }
+if (!filesPath) {
+  console.log('Please provide a filesPath, I dont know what to import else..');
+  process.exit()
+}
 
-  checkDatabase()
+try {
+  fs.existsSync('/splitted')
+} catch (e) {
+  console.log("Given File does not exist or is not readable!");
+  process.exit();
+}
 
-  function checkDatabase() {
-    db.listDatabases()
-      .then(names => {
-        console.log(names)
-        if (names.includes(config.database)) {
-          log('db already exists')
-          checkCollection()
-        } else {
-          log('db doesnt exist. creating...')
-          createDatabase()
-        }
-      });
-  }
+checkDatabase()
 
-  // Ensure, the db exists
-  function createDatabase() {
-    db.createDatabase(config.database, [{username: 'root'}], function (err, info) {
-      if (err) console.error(err.stack);
-      else {
-        console.log('created database')
+function checkDatabase() {
+  db.listDatabases()
+    .then(names => {
+      if (names.includes(config.database)) {
+        log('db already exists')
         checkCollection()
-      }
-    });
-  }
-
-  function checkCollection() {
-    db.useDatabase(config.database);
-    db.listCollections()
-    .then(collections => {
-      console.log(collections)
-      if (_.findIndex(collections, { "name": config.collection }) < 0) {
-        log('collection doesnt exist. creating...')
-        createCollection()
       } else {
-        log('collection already exists')
-        createIndices()
+        log('db doesnt exist. creating...')
+        createDatabase()
       }
     });
-  }
+}
 
-  function createCollection() {
-    let collection = db.collection(config.collection);
-    collection.create()
-    .then(() => {
-      // Skip for Testing Purposes
-      createIndices()
-      //ximportDump()
-    });
-  }
-
-  function createIndices() {
-    log('creating indices')
-    log(config.indices)
-    let collection = db.collection(config.collection);
-
-    async.each(config.indices, function(index, callback) {
-      if (index.type === "fulltext") {
-        collection.createFulltextIndex(index.fields, 3)
-        .catch(function(e) {
-          log(chalk.black.bgRed.bold('Error creating Fulltext index for ' + index.fields))
-          console.log(e);
-          process.exit()
-        })        
-        .then(index => {
-          log(chalk.black.bgGreen.bold('Fulltext Index has been created for ' + index.fields))
-          callback()
-        });
-      } else if (index.type === "skiplist") {
-        log(index.fields)
-        collection.createSkipList(index.fields)
-        .catch(function(e) {
-          log(chalk.black.bgRed.bold('Error creating Skiplist index for ' + index.fields))
-          log(e);
-          process.exit()
-        })
-        .then(index => {
-          log(chalk.black.bgGreen.bold('Skiplist Index has been created for ' + index.fields))
-          callback()
-        });
-      }
-    }, function(err) {
-        if( err ) {
-          log('A index failed to create');
-        } else {
-          log(chalk.black.bgGreen.bold('All indices have been created successfully'));
-          importDump()
-        }
-    });
-  }
-
-  function importDump() {
-    var files = fs.readdirSync(filesPath);
-
-    for (var file of files) {
-      exec('docker-compose exec arangodb arangoimp --file /parsed/' + file + ' --server.database ' + config.database + ' --server.password ' + config.password + ' --collection ' + config.collection + ' --create-collection true');
-      log(chalk.black.bgGreen.bold(`Import of ${file} finished! :-)`));
+// Ensure, the db exists
+function createDatabase() {
+  db.createDatabase(config.database, [{username: 'root'}], function (err, info) {
+    if (err) console.error(err.stack);
+    else {
+      console.log('created database')
+      checkCollection()
     }
+  });
+}
+
+function checkCollection() {
+  db.useDatabase(config.database);
+  db.listCollections()
+  .then(collections => {
+    if (_.findIndex(collections, { "name": config.collection }) < 0) {
+      log('collection doesnt exist. creating...')
+      createCollection()
+    } else {
+      log('collection already exists')
+      createIndices()
+    }
+  });
+}
+
+function createCollection() {
+  let collection = db.collection(config.collection);
+  collection.create()
+  .then(() => {
+    // Skip for Testing Purposes
+    createIndices()
+    //ximportDump()
+  });
+}
+
+function createIndices() {
+  log('creating indices')
+  let collection = db.collection(config.collection);
+
+  async.each(config.indices, function(index, callback) {
+    if (index.type === "fulltext") {
+      collection.createFulltextIndex(index.fields, 3)
+      .catch(function(e) {
+        log(chalk.black.red.bold('Error creating Fulltext index for ' + index.fields))
+        console.log(e);
+        process.exit()
+      })        
+      .then(index => {
+        log(chalk.black.green.bold('Fulltext Index has been created for ' + index.fields))
+        callback()
+      });
+    } else if (index.type === "skiplist") {
+      collection.createSkipList(index.fields)
+      .catch(function(e) {
+        log(chalk.black.red.bold('Error creating Skiplist index for ' + index.fields))
+        log(e);
+        process.exit()
+      })
+      .then(index => {
+        log(chalk.black.green.bold('Skiplist Index has been created for ' + index.fields))
+        callback()
+      });
+    }
+  }, function(err) {
+      if( err ) {
+        log('A index failed to create');
+      } else {
+        log(chalk.black.green.bold('All indices have been created successfully'));
+        importDump()
+      }
+  });
+}
+
+function importDump() {
+  var files = []
+
+  try {
+    files = fs.readdirSync(filesPath);
+  } catch (e) {
+    console.log('Problem beim Lesen des Ordners')
+  }
+
+  console.log(files)
+
+  for (var file of files) {
+    exec('docker-compose exec -T arangodb arangoimp --file /parsed/' + file + ' --server.database ' 
+          + config.database + ' --server.password ' + config.password 
+          + ' --collection ' + config.collection + ' --create-collection true');
+    log(chalk.black.bgGreen.bold(`Import of ${file} finished! :-)`));
   }
 }
 
-else {
- console.log('Please provide a filesPath, I dont know what to import else..');
-}
